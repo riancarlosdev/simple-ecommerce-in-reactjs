8027ffbdae52a6b03545f5ac17a3904b421819cf
riancarlosdev <riancontatoprofissional@gmail.com>
2022-09-23 00:45:30 -0300

    add info top

--- src/routes/checkout/Checkout.jsx
+++ src/routes/checkout/Checkout.jsx
@@ -1,4 +1,6 @@
 import React, { useContext } from "react";
+import Button from "../../components/button/Button";
+import FormInput from "../../components/form-input/FormInput";
 import { CartContext } from "../../context/cart.context";
 import "./checkout.styles.scss";
 
@@ -7,16 +9,24 @@ function Checkout() {
     useContext(CartContext);
 
   return (
-    <div>
+    <div className="container-grid">
+      <FormInput
+        label="confirmPassword"
+        inputOptions={{
+          type: "password",
+          name: "confirmPassword",
+          required: true,
+          value: "",
+        }}
+      />
+      <Button children={"SIGN UP"} type="submit" />
       <div>
         {cartItems.map((cartItem) => {
           return (
             <div key={cartItem.id}>
               <h2>{cartItem.name}</h2>
               <span>{cartItem.quantity}</span>
-              <span onClick={() => removeItemToCart(cartItem)}>
-                decrement
-              </span>
+              <span onClick={() => removeItemToCart(cartItem)}>decrement</span>
               <span onClick={() => addItemToCart(cartItem)}>increment</span>
             </div>
           );
--- src/routes/layout/Layout.jsx
+++ src/routes/layout/Layout.jsx
@@ -1,25 +1,55 @@
-import React, { useContext, useState } from "react";
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import { Outlet, Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Outlet } from "react-router-dom";
 import "./navbar.styles.scss";
-import { signOutUser } from "../../utils/firebase.utils";
+import { useLocation } from "react-router-dom";
+
 import CartIcon from "../../components/cart-icon/CartIcon";
 import CartDropDown from "../../components/cart-dropdown/CartDropDown";
-
 import { UserContext } from "../../context/user.context";
 import { CartContext } from "../../context/cart.context";
 import { LogoCompany } from "../../components/logo-company";
+import { TbCircle1, TbCircle2, TbCircle3 } from "react-icons/tb";
+import { BsCheckCircleFill } from "react-icons/bs";
 
 export const Layout = () => {
   const { currentUser } = useContext(UserContext);
   const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const path = useLocation().pathname;
 
   return (
     <>
       <div className="navigation">
         <LogoCompany />
-        <CartIcon />
-        {isCartOpen && <CartDropDown />}
+        {path !== "/checkout" && (
+          <>
+            <CartIcon />
+            {isCartOpen && <CartDropDown />}
+          </>
+        )}
+        {path === "/checkout" && (
+          <div id="container-position-check">
+            <div className="item-check">
+              <BsCheckCircleFill color="#4bb80c" size={29} />
+              <div className="value">
+                <p>Shopping Cart</p>
+                <div className="line" />
+              </div>
+            </div>
+            <div className="item-check">
+              <TbCircle2 size={35} color="#4bb80c" />
+              <div className="value">
+                <p>Checkout</p>
+                <div style={{ background: "#adacac" }} className="line" />
+              </div>
+            </div>
+            <div className="item-check finish">
+              <TbCircle3 color="#cccccc" size={30} />
+              <div className="value">
+                <p style={{ color: "#c2c2c2", fontWeight: 300 }}>Finish</p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
       <Outlet />
     </>
